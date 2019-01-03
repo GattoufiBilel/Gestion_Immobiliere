d0f0679a483ee60b83853785de9722c4a3dacba3
Gattoufi Bilel <43624051+GattoufiBilel@users.noreply.github.com>
2019-01-03 12:20:41 +0100

    Delete info.php

--- info.php
+++
@@ -1,7 +0,0 @@
-<?php
-    
-    echo " Opperation Effectue avec succes <br> <br> "; 
-    
-    echo " Clique vers la liste >  > > ";
-    echo "<a href= View/Appartement/liste.php > <input type=button value=Liste></a> "  ; 
-
